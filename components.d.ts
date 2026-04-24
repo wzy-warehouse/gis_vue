ac36e9ada33cde1325d1ee437dd82925c7bda930
zzw <2029503428@qq.com>
2026-04-24 13:18:08 +0800

    地点搜索

--- components.d.ts
+++ components.d.ts
@@ -11,6 +11,7 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
+    ElAutocomplete: typeof import('element-plus/es')['ElAutocomplete']
     ElButton: typeof import('element-plus/es')['ElButton']
     ElCheckbox: typeof import('element-plus/es')['ElCheckbox']
     ElCol: typeof import('element-plus/es')['ElCol']
