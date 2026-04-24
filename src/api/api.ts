960b1975c80c1169f6c84191979fdc315bc35081
3090 <507140388@qq.com>
2026-04-24 11:24:24 +0800

    feat: load user location after app ready

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,5 @@
 import { getSm2PublicKey } from './crypto';
+import { getAddressByCoordinate, getLocationByIp } from './location';
 
 /**
  * API接口统一导出对象
@@ -11,5 +12,19 @@ export const $api = {
      * @returns SM2公钥响应
      */
     getSm2PublicKey: () => getSm2PublicKey(),
+  },
+
+  // 定位模块
+  location: {
+    /**
+     * 根据IP获取当前所在地
+     */
+    getLocationByIp: () => getLocationByIp(),
+
+    /**
+     * 根据经纬度获取地址
+     */
+    getAddressByCoordinate: (lon: number, lat: number) =>
+      getAddressByCoordinate(lon, lat),
   },
 };
